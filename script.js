b5f25e92d1d502eff4d3f877220591ae710d7118
eldonaukh <eldonakh@gmail.com>
2025-01-24 01:22:48 +0800

    Add square side button and refactor createBoard in js

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 function main() {
-  const container = document.querySelector("#container");
-  let side = 16;
-  for (let i = 0; i < side; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
+  function createBoard(side) {
+    container.innerHTML = "";
 
-    for (let j = 0; j < side; j++) {
-      const grid = document.createElement("div");
-      grid.classList.add("grid");
-      row.appendChild(grid);
+    for (let i = 0; i < side; i++) {
+      const row = document.createElement("div");
+      row.classList.add("row");
+
+      for (let j = 0; j < side; j++) {
+        const grid = document.createElement("div");
+        grid.classList.add("grid");
+        row.appendChild(grid);
+      }
+
+      container.appendChild(row);
     }
-
-    container.appendChild(row);
   }
+  const topDiv = document.createElement("div");
+  const container = document.querySelector("#container");
+  const pageTitle = document.createElement("h1");
+  const promptBtn = document.createElement("button");
+  const currentSide = document.createElement("h2");
+  document.body.insertBefore(topDiv, container);
+  pageTitle.innerText = "Etch-aSketch";
+  promptBtn.innerText = "Update number of squares per side";
+  topDiv.appendChild(pageTitle);
+  topDiv.appendChild(currentSide);
+  topDiv.appendChild(promptBtn);
+  let side = 16;
+  currentSide.innerText = `Current side: ${side}`;
+  createBoard(side);
+  promptBtn.addEventListener("click", () => {
+    let ans = prompt("Please enter number of squares per side: ");
+    if (ans > 100) {
+      side = 100;
+    } else if (ans < 1) {
+      side = 1;
+    } else {
+      side = parseInt(ans);
+    }
+    currentSide.innerText = `Current side: ${side}`;
+    createBoard(side);
+  });
 
   container.addEventListener("mouseover", (evt) => {
     if (evt.target.className === "grid") {
