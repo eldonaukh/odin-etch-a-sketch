edda4bfbacfaaf3969d9093ccf716a1e37bbcae9
eldonaukh <eldonakh@gmail.com>
2025-01-23 18:28:44 +0800

    Add html, js, css

---
+++ script.js
@@ -0,0 +1,18 @@
+function main() {
+  const container = document.querySelector("#container");
+  let side = 16;
+  for (let i = 0; i < side; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+
+    for (let j = 0; j < side; i++) {
+      const grid = document.createElement("div");
+      grid.classList.add("grid");
+      row.appendChild(grid);
+    }
+
+    container.appendChild(row);
+  }
+}
+
+main();
